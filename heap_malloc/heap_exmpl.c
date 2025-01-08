e557f10485bfff865d54b74bbcc1c12095c431e1
minnesang <minnesangDev@gmail.com>
2025-01-09 00:44:55 +0200

    updated heap_malloc

--- heap_malloc/heap_exmpl.c
+++ heap_malloc/heap_exmpl.c
@@ -2,12 +2,7 @@
 #include<string.h>
 #include<stdlib.h>
 
-void ifMallocTrue(void *var_ptr){
-    if(var_ptr == NULL){
-        fprintf(stderr, "Fault, the memory from heap cant be malloc\n");
-        exit(-1);
-    }
-}
+void *ifMallocTrue(unsigned int);
 
 int main(int argc, char *argv[]) {
 
@@ -23,17 +18,13 @@ int main(int argc, char *argv[]) {
     }
 
     printf("\t[+] malloc %d bytes from heap for variable char_ptr\n", memory_size);
-    char_ptr = (char *) malloc(memory_size);
-
-    ifMallocTrue(char_ptr);
+    char_ptr = (char *) ifMallocTrue(memory_size);
 
     strcpy(char_ptr, "This memory are located in heap\n");
     printf("char_ptr %p -> %s\n", char_ptr, char_ptr);
 
     printf("\t[+] malloc 12 bytes from heap to variable int_ptr\n");
-    int_ptr = (int *) malloc(12);
-
-    ifMallocTrue(int_ptr);
+    int_ptr = (int *) ifMallocTrue(12);
 
     *int_ptr = 31337;
     printf("int_ptr %p -> %d\n", int_ptr, *int_ptr);
@@ -42,9 +33,8 @@ int main(int argc, char *argv[]) {
     free(char_ptr);
 
     printf("\t[+] malloc another 15 bytes from heap to variable char_ptr\n");
-    char_ptr = (char *) malloc(15);
+    char_ptr = (char *) ifMallocTrue(15);
 
-    ifMallocTrue(char_ptr);
     strcpy(char_ptr, "new memory\n");
 
     printf("char_ptr %p -> %s\n", char_ptr, char_ptr);
@@ -56,4 +46,16 @@ int main(int argc, char *argv[]) {
     free(char_ptr);
 
     return 0;
+}
+
+
+void *ifMallocTrue(unsigned int size){
+    void *var_ptr;
+    var_ptr = malloc(size);
+
+    if(var_ptr == NULL){
+        fprintf(stderr, "Fault, the memory from heap cant be malloc\n");
+        exit(-1);
+    }
+    return var_ptr;
 }
