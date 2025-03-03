d9a345b13367cfceab94c89cce58628e4d318e43
minnesang <minnesangDev@gmail.com>
2025-03-03 20:45:28 +0200

    added web server pracs + headers

---
+++ headers/hacking.h
@@ -0,0 +1,42 @@
+#include <stddef.h>
+void fatal(char *message){
+        char error_message[100];
+
+        strcpy(error_message, "[!] Critical error!");
+        strncat(error_message, message, 83);
+        perror(error_message);
+        exit(-1);
+}
+
+void *updatedMalloc(unsigned int size){
+        void *ptr;
+        ptr = malloc(size);
+        if(ptr == NULL){
+                fatal("In function updatedMalloc because of malloc bytes");
+        }
+        return ptr;
+}
+
+void dump(const unsigned char *data_buffer, const unsigned int length){
+        unsigned char byte;
+        unsigned int i, j;
+        for(i = 0; i < length; i++){
+                byte = data_buffer[i];
+                printf("%02x ", data_buffer[i]);
+
+                if(((i%16) == 15) || (i == length - 1)) {
+                        for(j = 0; j < 15 - (i%16); j++)
+                                printf(" ");
+                        printf("| ");
+                        for(j = (i-(i%16));j <= i; j++){
+                                byte = data_buffer[j];
+                                if((byte > 31) && (byte < 127))
+                                        printf("%c", byte);
+                                else 
+                                        printf(".");
+                        }
+                        printf("\n");
+                        
+                }
+        }
+}
---
+++ headers/hacking_network.h
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+typedef unsigned char uchar;
+
+int send_string(int sock_fd, uchar *buffer){
+    int sent_bytes, bytes_to_send;
+    bytes_to_send = strlen((char *)buffer);
+
+    while(bytes_to_send > 0){
+        sent_bytes = send(sock_fd, buffer, bytes_to_send, 0);
+        if(sent_bytes == -1) return 0;
+
+        bytes_to_send -= sent_bytes;
+        buffer += sent_bytes;
+    }
+
+    return 1;
+}
+
+int recv_line(int sock_fd, uchar *dest_buffer){
+#define EOL "\r\n"
+#define EOL_SIZE 2
+
+    uchar *ptr;
+    int eol_matched = 0;
+
+    ptr = dest_buffer;
+    while(recv(sock_fd, ptr, 1, 0) == 1){
+        if(*ptr == EOL[eol_matched]){
+            eol_matched++;
+
+            if(eol_matched == EOL_SIZE){
+                *(ptr + 1 - EOL_SIZE) = '\0';
+                return strlen((char *)dest_buffer);
+
+            }
+        } else {
+            eol_matched = 0;
+        }
+        ptr++;
+    }
+    return 0;
+}
